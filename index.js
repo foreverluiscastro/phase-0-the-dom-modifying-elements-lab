3cfe8c21b8e0bf200998c17e41347b4115a50d16
luis <foreverluiscastro@gmail.com>
2021-03-07 19:20:33 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+document.body.removeChild(main);
+
+const newHeader = document.createElement('h1');
+
+document.body.appendChild(newHeader);
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.innerHTML = "<h1>Luis is the champion</h1>";
